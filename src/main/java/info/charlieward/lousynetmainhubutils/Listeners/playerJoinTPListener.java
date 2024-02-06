de7dfde6dd0ba0d4e92d93e5ab332f65422b475c
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-06 17:03:00 +0000

    Added tp on spawn

--- src/main/java/info/charlieward/lousynetmainhubutils/Listeners/playerJoinTPListener.java
+++ src/main/java/info/charlieward/lousynetmainhubutils/Listeners/playerJoinTPListener.java
@@ -1,2 +1,18 @@
-package info.charlieward.lousynetmainhubutils.Listeners;public class playerJoinTPListener {
+package info.charlieward.lousynetmainhubutils.Listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class playerJoinTPListener implements Listener {
+
+    Location spawn = new Location(Bukkit.getWorld("hub"),-74,35,-7);
+    @EventHandler
+    public void PlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        player.teleport(spawn);
+    }
 }
--- src/main/java/info/charlieward/lousynetmainhubutils/LousyNetMainHubUtils.java
+++ src/main/java/info/charlieward/lousynetmainhubutils/LousyNetMainHubUtils.java
@@ -1,5 +1,6 @@
 package info.charlieward.lousynetmainhubutils;
 
+import info.charlieward.lousynetmainhubutils.Listeners.playerJoinTPListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LousyNetMainHubUtils extends JavaPlugin {
@@ -7,7 +8,9 @@ public final class LousyNetMainHubUtils extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Plugin Online");
 
+        getServer().getPluginManager().registerEvents(new playerJoinTPListener(), this);
     }
 
     @Override
