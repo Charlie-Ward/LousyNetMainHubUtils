1456c0ed2b46f2b97f9b9c478d8b590596865fd2
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-07 18:59:24 +0000

    Might help if i make it able to register events

--- src/main/java/info/charlieward/lousynetmainhubutils/LousyNetMainHubUtils.java
+++ src/main/java/info/charlieward/lousynetmainhubutils/LousyNetMainHubUtils.java
@@ -1,6 +1,7 @@
 package info.charlieward.lousynetmainhubutils;
 
 import info.charlieward.lousynetmainhubutils.Listeners.dropBelowYLevel5;
+import info.charlieward.lousynetmainhubutils.Listeners.playerDeathDropCancel;
 import info.charlieward.lousynetmainhubutils.Listeners.playerJoinTPListener;
 import info.charlieward.lousynetmainhubutils.Listeners.playerRespawnTPListener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,6 +16,7 @@ public final class LousyNetMainHubUtils extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new playerJoinTPListener(), this);
         getServer().getPluginManager().registerEvents(new dropBelowYLevel5(), this);
         getServer().getPluginManager().registerEvents(new playerRespawnTPListener(), this);
+        getServer().getPluginManager().registerEvents(new playerDeathDropCancel(), this);
     }
 
     @Override
