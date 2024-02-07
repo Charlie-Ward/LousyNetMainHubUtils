01d1c1feecb35076e0ad4a462b3a862e7fccd6ca
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-07 18:55:13 +0000

    Removed death drops

---
+++ src/main/java/info/charlieward/lousynetmainhubutils/Listeners/playerDeathDropCancel.java
@@ -0,0 +1,13 @@
+package info.charlieward.lousynetmainhubutils.Listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class playerDeathDropCancel implements Listener {
+
+    @EventHandler
+    public void onDeathDrop(PlayerDeathEvent event) {
+        event.getDrops().clear();
+    }
+}
