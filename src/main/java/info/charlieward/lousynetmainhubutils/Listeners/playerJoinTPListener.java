c0688f9a6541158c674ae0645ebc0112c465492e
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-08 18:04:24 +0000

    added the compass on join

--- src/main/java/info/charlieward/lousynetmainhubutils/Listeners/playerJoinTPListener.java
+++ src/main/java/info/charlieward/lousynetmainhubutils/Listeners/playerJoinTPListener.java
@@ -1,13 +1,15 @@
 package info.charlieward.lousynetmainhubutils.Listeners;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 public class playerJoinTPListener implements Listener {
 
@@ -20,5 +22,19 @@ public class playerJoinTPListener implements Listener {
         if (!player.hasPermission("lousyNetMainHubUtils.enforceGamemode")){
             player.setGameMode(GameMode.ADVENTURE);
         }
+
+        ItemStack item = new ItemStack(Material.COMPASS, 1);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName(ChatColor.GREEN + "Game Selector");
+
+        ArrayList<String> lore = new ArrayList<String>();
+        lore.add("");
+        lore.add(ChatColor.GRAY + "Right Click this compass to choose which gamemode you want to go to");
+
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+        Inventory inventory = player.getInventory();
+        inventory.setItem(4, item);
     }
 }
