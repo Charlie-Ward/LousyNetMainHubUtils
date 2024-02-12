b1206810fdf42a0b2d989652b601115b29bddb26
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-12 11:28:33 +0000

    made a complete api call

--- src/main/java/info/charlieward/lousynetmainhubutils/call.java
+++ src/main/java/info/charlieward/lousynetmainhubutils/call.java
@@ -5,9 +5,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
 
 public class call {
     public static void main(String[] args) {
+
+        String type = "playerCount";
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.mcsrvstat.us/3/194.163.179.210:25566"))
                 .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -22,7 +26,22 @@ public class call {
             e.printStackTrace();
         }
 
-        System.out.println(response.body());
+
+        if (type.equals("playerCount")) {
+            String[] split = response.body().split("online", 2);
+            String[] split2 = split[1].split(":", 2);
+
+            if (split2[1].contains("max")) {
+                String[] split3 = split2[1].split(",", 2);
+                System.out.println(split3[0]);
+            } else {
+                System.out.println("Server Offline");
+            }
+        }
+
     }
 
 }
+
+
+//https://api.mcsrvstat.us/3/194.163.179.210:25566
