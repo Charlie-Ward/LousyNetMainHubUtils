183e05f85e46c4e1776a71cb373cf8ef7f7f6574
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-12 11:29:13 +0000

    added the api call to the gamemode item also made it so the gamemodeItem could be made through a function to stop repetition of code

--- src/main/java/info/charlieward/lousynetmainhubutils/Listeners/gamemodeSelector.java
+++ src/main/java/info/charlieward/lousynetmainhubutils/Listeners/gamemodeSelector.java
@@ -1,23 +1,24 @@
 package info.charlieward.lousynetmainhubutils.Listeners;
 
+import com.sun.jdi.CharType;
 import info.charlieward.lousynetmainhubutils.LousyNetMainHubUtils;
 import org.bukkit.*;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.persistence.PersistentDataType;
-import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.UUID;
 
 public class gamemodeSelector implements Listener {
 
@@ -73,20 +74,7 @@ public class gamemodeSelector implements Listener {
         gameSelector.setItem(8, createFiller());
 
         gameSelector.setItem(9, createFiller());
-
-        ItemStack survivalSelector = new ItemStack(Material.OAK_LOG);
-        ItemMeta survivalSelectorMeta = survivalSelector.getItemMeta();
-        survivalSelectorMeta.setDisplayName(ChatColor.GOLD + "Survival");
-        ArrayList<String> survivalLore = new ArrayList<String>();
-        survivalLore.add("");
-        survivalLore.add(ChatColor.GRAY + "Online Players: WIP");
-        survivalLore.add(ChatColor.GRAY + "MC Version: WIP");
-        survivalLore.add("");
-        survivalLore.add(ChatColor.GRAY + "Hop onto the survival server and make magnificent bases and super farms");
-        survivalLore.add("");
-        survivalSelectorMeta.setLore(survivalLore);
-        survivalSelector.setItemMeta(survivalSelectorMeta);
-        gameSelector.setItem(10, survivalSelector);
+        gameSelector.setItem(10, makeGamemodeItem(Material.DIAMOND_BLOCK, "Survival", "https://api.mcsrvstat.us/3/194.163.179.210:25566", "1.17", "Hop onto the survival server and make magnificent bases and super farms"));
 
         player.openInventory(gameSelector);
     }
@@ -99,4 +87,57 @@ public class gamemodeSelector implements Listener {
         Filler.setItemMeta(fMeta);
         return Filler;
     }
+
+    private static String getServerCount(String URL) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        String[] split = response.body().split("online", 2);
+        String[] split2 = split[1].split(":", 2);
+
+        if (split2[1].contains("max")) {
+            String[] split3 = split2[1].split(",", 2);
+            return split3[0];
+        } else {
+            return "Server Offline";
+        }
+    }
+
+    public static ItemStack makeGamemodeItem(Material blockItem, String gamemodeName, String URL, String serverMCversion, String description) {
+        String serverInfo = getServerCount(URL);
+
+        ItemStack item = new ItemStack(blockItem);
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName(ChatColor.GOLD + gamemodeName);
+        ArrayList<String> itemLore = new ArrayList<String>();
+        itemLore.add("");
+        itemLore.add(ChatColor.WHITE + "Players" + ChatColor.GRAY + serverInfo);
+        itemLore.add(ChatColor.WHITE + "Minecraft Version" + ChatColor.GRAY + serverMCversion);
+        itemLore.add("");
+        itemLore.add(ChatColor.GRAY + description);
+        itemLore.add("");
+        if (serverInfo.equals("Server Offline")) {
+            itemLore.add(ChatColor.RED + "Server Offline");
+        } else {
+            itemLore.add(ChatColor.GREEN + "Play Now!");
+        }
+        itemLore.add("");
+        itemMeta.setLore(itemLore);
+        item.setItemMeta(itemMeta);
+        return item;
+    }
 }
+
+//"https://api.mcsrvstat.us/3/194.163.179.210:25566"
