1649481a1f4664b31b0f36e9ef5702ecb22b073f
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-13 13:30:09 +0000

    Added fillers on respawn

--- src/main/java/info/charlieward/lousynetmainhubutils/Listeners/playerRespawnTPListener.java
+++ src/main/java/info/charlieward/lousynetmainhubutils/Listeners/playerRespawnTPListener.java
@@ -1,9 +1,7 @@
 package info.charlieward.lousynetmainhubutils.Listeners;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import info.charlieward.lousynetmainhubutils.LousyNetMainHubUtils;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,6 +10,7 @@ import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
 
@@ -19,24 +18,50 @@ public class playerRespawnTPListener implements Listener {
 
     Location spawn = new Location(Bukkit.getWorld("hub"),-74.5,35,-7.5,90,0);
     @EventHandler
-    public void onRespawn(PlayerRespawnEvent event){
-        event.setRespawnLocation(spawn);
-        event.getPlayer().getInventory().setHeldItemSlot(2);
+    public void onRespawn(PlayerRespawnEvent e){
+        e.setRespawnLocation(spawn);
+        e.getPlayer().getInventory().setHeldItemSlot(2);
 
-        Player player = event.getPlayer();
+        Player player = e.getPlayer();
 
-        ItemStack item = new ItemStack(Material.COMPASS, 1);
-        ItemMeta meta = item.getItemMeta();
-
-        meta.setDisplayName(ChatColor.GREEN + "Game Selector");
-
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("");
-        lore.add(ChatColor.GRAY + "Right Click this compass to choose which gamemode you want to go to");
-
-        meta.setLore(lore);
-        item.setItemMeta(meta);
+        ItemStack gameSelectorCompass = new ItemStack(Material.COMPASS, 1);
+        ItemMeta gameSelectorCompassItemMeta = gameSelectorCompass.getItemMeta();
+        gameSelectorCompassItemMeta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "LousyNet Game Selector");
+        ArrayList<String> gameSelectorCompassLore = new ArrayList<String>();
+        gameSelectorCompassLore.add("");
+        gameSelectorCompassLore.add(ChatColor.GRAY + "Right Click to access the game selector");
+        gameSelectorCompassLore.add("");
+        gameSelectorCompassItemMeta.setLore(gameSelectorCompassLore);
+        gameSelectorCompass.setItemMeta(gameSelectorCompassItemMeta);
         Inventory inventory = player.getInventory();
-        inventory.setItem(4, item);
+        inventory.setItem(2, gameSelectorCompass);
+
+        ItemStack gameSelectorSocial = new ItemStack(Material.PAPER, 1);
+        ItemMeta gameSelectorSocialMeta = gameSelectorSocial.getItemMeta();
+        gameSelectorSocialMeta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Social Menu - WIP");
+        ArrayList<String> gameSelectorSocialLore = new ArrayList<String>();
+        gameSelectorSocialLore.add("");
+        gameSelectorSocialLore.add(ChatColor.GRAY + "Right click to access the social menu");
+        gameSelectorSocialLore.add("");
+        gameSelectorSocialMeta.setLore(gameSelectorSocialLore);
+        gameSelectorSocial.setItemMeta(gameSelectorSocialMeta);
+        inventory.setItem(6, gameSelectorSocial);
+
+
+
+        for(int count = 0; count < 44; count ++) {
+            inventory.addItem(createFiller());
+        }
+
+        e.getPlayer().getInventory().setHeldItemSlot(2);
+    }
+
+    private static ItemStack createFiller() {
+        ItemStack Filler = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+        ItemMeta fMeta = Filler.getItemMeta();
+        fMeta.setDisplayName(ChatColor.GRAY + "");
+        fMeta.getPersistentDataContainer().set(new NamespacedKey(LousyNetMainHubUtils.getPlugin(), "unique"), PersistentDataType.DOUBLE, Math.random());
+        Filler.setItemMeta(fMeta);
+        return Filler;
     }
 }
